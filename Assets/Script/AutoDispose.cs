defb0c344857da31c35bb90a4d0753b3b689a83d
Назар <wowpes7@mail.ru>
2026-07-27 18:18:45 +0300

    refactor: make auto-dispose pool aware

--- Assets/Script/AutoDispose.cs
+++ Assets/Script/AutoDispose.cs
@@ -1,11 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
 public sealed class AutoDispose : MonoBehaviour
 {
     [SerializeField, Min(0f)] private float timeToLive = 5f;
 
-    private void Start()
+    private Coroutine releaseRoutine;
+
+    private void OnEnable()
     {
-        Destroy(gameObject, timeToLive);
+        releaseRoutine = StartCoroutine(ReleaseAfterLifetime());
+    }
+
+    private void OnDisable()
+    {
+        if (releaseRoutine != null)
+        {
+            StopCoroutine(releaseRoutine);
+            releaseRoutine = null;
+        }
+    }
+
+    private IEnumerator ReleaseAfterLifetime()
+    {
+        if (timeToLive > 0f)
+        {
+            yield return new WaitForSeconds(timeToLive);
+        }
+
+        releaseRoutine = null;
+        GlobalSystem system = GlobalSystem.Instance;
+        if (system != null)
+        {
+            system.Release(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
